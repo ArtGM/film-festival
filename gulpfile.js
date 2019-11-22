b91d6dac8dab6407eede553dfa6cf59e12e04332
ArtMG <gx.morisson@protonmail.com>
2019-11-22 10:33:31 +0100

    add a loader, change jquery version, remove unvalid comment

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,10 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const csscomb = require('gulp-csscomb')
 const autoprefixer = require('gulp-autoprefixer')
-const uglify = require('gulp-uglify')
-const babel = require('gulp-babel')
 const plumber = require('gulp-plumber')
 const browserSync = require('browser-sync')
 const sourcemaps = require('gulp-sourcemaps')
 const cleanCSS = require('gulp-clean-css')
-const imagemin = require('gulp-imagemin')
 
 gulp.task('sass', function () {
   return gulp
@@ -23,35 +20,6 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./assets/css'))
 })
 
-gulp.task('js', function () {
-  return gulp
-    .src('./assets/js/*.js')
-    .pipe(plumber())
-    .pipe(
-      babel({
-        presets: [ '@babel/env' ]
-      })
-    )
-    .pipe(uglify())
-    .pipe(gulp.dest('./js'))
-})
-
-gulp.task('img', () => {
-  return gulp
-    .src('./assets/img/src/**')
-    .pipe(
-      imagemin([
-        imagemin.gifsicle({ interlaced: true }),
-        imagemin.jpegtran({ progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
-        imagemin.svgo({
-          plugins: [ { removeViewBox: true }, { cleanupIDs: false } ]
-        })
-      ])
-    )
-    .pipe(gulp.dest('assets/img'))
-})
-
 gulp.task('watch', function () {
   browserSync.init({
     server: {
@@ -60,6 +28,6 @@ gulp.task('watch', function () {
   })
 
   gulp.watch('./assets/scss/**/**.*', gulp.series('sass')).on('change', browserSync.reload)
-  gulp.watch('./assets/js/**/**.*', gulp.series('js')).on('change', browserSync.reload)
+  gulp.watch('./assets/js/**/**.*').on('change', browserSync.reload)
   gulp.watch('./**/*.html').on('change', browserSync.reload)
 })
